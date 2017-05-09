33b883d0ff6c951375de20869fd705c9ff004e24
Abhishek Satpathy <abhisheksatpathy28@gmail.com>
2017-05-09 23:48:14 +1000

    CSS updates and Construction Mark

--- static/home.js
+++ static/home.js
@@ -5,5 +5,12 @@ $(function() {
         $("#overlay-nav-menu").toggleClass("is-open");
         $('[data-toggle-menu]').hide();
     });
+    $('.top-bar-left ul, a').hover(function(){
+    	$(this).find('.logo_black').show();
+    	$(this).find('.logo').hide();
+    }, function(){
+    	$(this).find('.logo_black').hide();
+    	$(this).find('.logo').show();
+    });
 
 });
